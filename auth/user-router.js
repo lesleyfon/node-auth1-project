b791ef455ef11711b64a62090e1fa371544781ca
lesley <lesleybanadzem2017@gmail.com>
2020-01-15 15:03:33 -0800

    created a register endpoint to add new user to the db and return the newly created user

--- auth/user-router.js
+++ auth/user-router.js
@@ -1,7 +1,27 @@
 const express = require('express');
 
 const router = express.Router();
-const {findUsers} = require('./../helper/userHelper');
+const {
+    findUsers,
+    addUser
+    } = require('./../helper/userHelper');
+
+
+router.post('/register', async (req, res, next) => {
+    const {username, password } = req.body;
+    if(!username || !password){
+        res.json({
+            message: 'username or password is undefined'
+        })
+    } else{
+        const user = await addUser({username, password });
+        res.status(200).json({
+            message:'Post successful',
+            user: user
+        })
+    }
+
+})
 
 router.get('/users', async (req, res, next)=>{
     const users = await findUsers();
--- helper/userHelper.js
+++ helper/userHelper.js
@@ -4,7 +4,16 @@ function findUsers(){
     return db('users')
 }
 
+function findUserById(id){
+    return db('users').where('id', id).first()
+}
+async function addUser(user){
+    const [id] = await db('users').insert(user);
+    return findUserById(id)
+}
 
 module.exports = {
-    findUsers
+    findUsers,
+    findUserById,
+    addUser
 }
