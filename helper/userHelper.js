5ed89a14988f80df92a77c23ae0dfd686452e225
lesley <lesleybanadzem2017@gmail.com>
2020-01-15 15:10:35 -0800

    wrote a hash in the addUser helper method to hash userpassword before adding it to the db

--- helper/userHelper.js
+++ helper/userHelper.js
@@ -1,4 +1,5 @@
 const db = require('./../data/dbConfig');
+const bcrypt = require('bcryptjs')
 
 function findUsers(){
     return db('users')
@@ -8,6 +9,9 @@ function findUserById(id){
     return db('users').where('id', id).first()
 }
 async function addUser(user){
+   const hash = await bcrypt.hash(user.password, 10);
+   user.password = hash;
+   console.log(hash)
     const [id] = await db('users').insert(user);
     return findUserById(id)
 }
